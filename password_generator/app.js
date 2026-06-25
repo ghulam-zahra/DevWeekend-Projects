a21b8d67ec07062a6841721a059d4eaccc56f37b
ghulam-zahra <ghulamzahra1504@gmail.com>
2026-06-25 10:12:01 +0500

    Generate a random password using js

---
+++ password_generator/app.js
@@ -0,0 +1,27 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while (password.length < length) {
+        const allCharacters = upperCase + lowerCase + numbers + symbols;
+        password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand("copy");
+    alert("Password copied to clipboard!");
+}
